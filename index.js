0e33ab861660d79999aaff28324503075d71a324
Terell <Terelld@gmail.com>
2022-05-23 15:18:58 -0500

    commit tree 5cbe5948b302bacadeb49020ad8c64c4e76969ea

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function(plans = "go to the office") {
+    return `This Monday, I will ${plans}.`;
+}
+
+function wrapAdjective(visFlare = '*') {
+    return function innerFunction( adjective = 'special') {
+        return `You are ${visFlare}${adjective}${visFlare}!`;
+} 
+}
+
+    // return `You are ${special}${adjective}${special}!`;
+
+// function extraNice("||", "a dedicated programmer");
+// const wrappedAdjective() {
+// return `You are ${special1}${adjective1}${special1}!
+// }
